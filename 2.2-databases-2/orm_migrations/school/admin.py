91867941fbac2133b0abfb7cdc36b3e63ab006a3
Vladimir Vasilenko <vladoborowsky@gmail.com>
2022-12-15 23:12:07 +0500

    add homework 4

---
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Student, Teacher
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'id', 'group']
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['name', 'id', 'subject']
+
